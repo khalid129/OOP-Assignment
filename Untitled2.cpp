be68e4e06fff4f079dc63ea700ae0bfa5a7b606d
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-19 19:26:57 +0500

    OOP Assignment

---
+++ Untitled2.cpp
@@ -0,0 +1,16 @@
+#include<ioatream>
+#include<conio.h>
+using namespace std;
+class person
+{
+protected:
+char name[;
+int age;
+char gender;
+public:
+	person()
+};()
+class student:public person
+{
+	
+}
