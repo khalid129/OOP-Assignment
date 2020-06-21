0da877a9a6474473276f2cd876807d216c3ae390
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-21 09:15:11 +0500

    OOP Assignment

---
+++ week2.5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{ int a;
+cout<<"Enter a two digit number";
+cin>>a;cout<<endl;
+cout<<"The enter number is "<<a<<endl;
+cout<<"The swaping of the number is "<<a%10<<a/10;
+getch();  return 0; }
+
