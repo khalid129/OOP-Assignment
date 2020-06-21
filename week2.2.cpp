6095955475114b6ce2b49ce31601be61b6794ac1
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-21 09:12:27 +0500

    OOP Assignment

---
+++ week2.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{ int inches;
+cout<<"enter a height in inches";
+cin>>inches;
+cout<<"the height is ";
+int a=inches/12;
+int b=inches%12;
+cout<<a<<"'"<<" "<<b<<"''"<< endl;
+return 0;  }
+
